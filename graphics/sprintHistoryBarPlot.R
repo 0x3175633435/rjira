065950c989d09396fc69231e31d87eea4345f14f
Lucas A. de Araújo <lucas.araujo@bevicred.com.br>
2018-04-19 11:56:53 -0300

    Conversão do sprintHistoryBarPlot para função

--- graphics/sprintHistoryBarPlot.R
+++ graphics/sprintHistoryBarPlot.R
@@ -1,16 +1,27 @@
 # Dependências
 source("~/ProjectManagement/bevicred-erp/functions/getSprints.R")
 
-# Busca as sprints
-sprints = getSprints("~/ProjectManagement/bevicred-erp/datasets/issues-19042018.csv")
+#'
+#' sprintHistoryBarPlot
+#' 
+#' Gera um gráfico de barras com histórico de sprints.
+#' 
+#' @seealso https://i.imgur.com/gHXYzQW.png
+#'
+sprintHistoryBarPlot <- function(){
+  # Busca as sprints
+  sprints = getSprints("~/ProjectManagement/bevicred-erp/datasets/issues-19042018.csv")
+  
+  # Gera o gráfico com as sprints
+  sprintHistoryBarPlot = barplot(sprints$x, 
+          names.arg = sprints$s, 
+          col       = rainbow(nrow(sprints)),
+          main      = "Histórico de Sprints",
+          ylab      = "Pontuação",
+          xlab      = "Sprints")
+  
+  # Adiciona número de pontos entregues ao gráfico
+  text(sprintHistoryBarPlot, sprints$x-10, sprints$x) 
+}
 
-# Gera o gráfico com as sprints
-sprintHistoryBarPlot = barplot(sprints$x, 
-        names.arg = sprints$s, 
-        col       = rainbow(nrow(sprints)),
-        main      = "Histórico de Sprints",
-        ylab      = "Pontuação",
-        xlab      = "Sprints")
-
-# Adiciona número de pontos entregues ao gráfico
-text(sprintHistoryBarPlot, sprints$x-10, sprints$x) 
+sprintHistoryBarPlot()
